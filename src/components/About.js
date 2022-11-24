1f98ed90a3730801c8234bb4f58c7ed3c1e12302
tetedecactus <o.labrecque.lacasse@gmail.com>
2022-11-24 18:05:16 -0500

    +++

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,16 @@
-
+import '../style/About.css'
 import olabrecq from '../assets/img/olabrecq.jpg'
 
 function About() {
 	return (
-		<div className='min-h-full bg-black py-16'>
+		<div className='min-h-full bg-black py-16 about-page'>
 			<div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
 				<div className='md:1/12 lg:1/12'>
 					<p className='mt-5 text-center text-[#FFFBDC]'>I am</p>
 				</div>
 				<div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
 					<div className="md:7/12 lg:w-8/12">
-						<h2 className="text-2xl text-[#FFFBDC] font-bold text-end md:text-4xl">Olivier Labrecque <br/>Lacasse</h2>
+						<h2 className="name text-2xl text-[#FFFBDC] font-bold md:text-4xl">Olivier Labrecque <br/>Lacasse</h2>
 					</div>
 					<div className="md:1/12 lg:w-6/12 justify-center">
 						<img className='rounded-md opacity-60' src={olabrecq} alt="me" width="250" height="250"/>
