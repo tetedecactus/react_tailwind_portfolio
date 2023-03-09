65b9d7e1ed56a493c7d5cb96a5bd9dfb5e02fcb6
tetedecactus <o.labrecque.lacasse@gmail.com>
2023-03-09 13:03:31 -0500

    to pull

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ import NavBar from "./NavBar";
 import SocialIcons from "./SocialIcons";
 import "../../src/App.scss";
 import "../style/Home.scss";
-import { useContext } from "react";
+import { useContext,useEffect } from "react";
 import { ThemeContext } from "../context/ThemeContext.js";
 import ToggleButton from "./ToggleButton";
 import ContactForm from "./ContactForm";
@@ -16,30 +16,18 @@ function HomePage() {
   // class Homepage extends React.Component {
     const { theme, setTheme } = useContext(ThemeContext);
 
-    const bouton = document.getElementById('But');
-
-	
-    console.log(bouton);
-    if (bouton) {
-      console.log(bouton);
-      console.log(theme);
-
-        bouton.addEventListener('click', function() {
-          if (theme == 'CSM') {
-            console.log('inside this 1st shit');
-            bouton.innerHTML = 'Dark Theme';
-          }
-          else if (theme == 'dark') {
-            console.log('inside this 2st shit');
-            console.log(bouton)
-            bouton.innerHTML = 'Light Theme';
-          }
-          else {
-            console.log('inside this 3st shit');
-            bouton.innerHTML = 'CHAINESAW MAN THEME';
-          }
-        });
-    }
+    // useEffect(() => {
+	// 	const bouton = document.getElementById('But');
+	// 	  bouton.addEventListener('click', function() {
+	// 		if (theme === 'CSM') {
+	// 		  bouton.innerHTML = 'Light Theme';
+	// 		} else if (theme === 'dark') {
+	// 		  bouton.innerHTML = 'CHAINESAW MAN THEME!!!!!';
+	// 		} else {
+	// 		  bouton.innerHTML = 'Dark Theme';
+	// 		}
+	// 	  });
+	//   }, []);
   
     return (
       <section id="themeButton" className={theme}>
@@ -51,7 +39,7 @@ function HomePage() {
           <ContactForm />
           <button
             id='But'
-            className="But relative border font-bold rounded-md px-4 py-4 mt-6 ml-14"
+            className="relative border font-bold rounded-md px-4 py-4 mt-6 ml-14"
             onClick={() =>
               setTheme(
                 theme === "light" ? "dark" : theme === "dark" ? "CSM" : "light"
